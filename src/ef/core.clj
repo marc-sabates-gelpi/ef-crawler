a68de7f2d1755b32e49cf9fc3df363baa20ba6ca
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-03-30 17:52:02 +0100

    Filter out shallower pages

--- src/ef/core.clj
+++ src/ef/core.clj
@@ -3,28 +3,24 @@
             [cemerick.url :as url])
   (:gen-class))
 ;;; C-c RET h d [com.cemerick/url "0.1.1"]
-;;; REVIEW: Non greedy patterns +? 
-#_(def ^:private github-url-pattern #"\"(https?://github.com/.+?)\"")
-#_(def ^:private twitter-url-pattern #"\"(https?://twitter.com/.+?)\"")
-;;; REVIEW: Lookahead patterns
 (def ^:private github-url-pattern #"https?://github.com/[^\"\s]+(?=\")")
 (def ^:private twitter-url-pattern #"https?://twitter.com/[^\"\s]+(?=\")")
 (def ^:private error-message "[Error] %s\n")
 
 (defn- scrape-page
-  "Gets the Twitter and Github urls from `html` content. "
+  "Gets the Twitter and Github urls from `html` content."
   [html]
-  (when html 
+  (when html
     (hash-map :github-url (re-find github-url-pattern html) 
               :twitter-url (re-find twitter-url-pattern html))))
 
 (defn- sanitise-url
-  "Returns the `url` without anchor and empty path."
+  "Returns the `url` without anchor and with empty path."
   [url]
   (-> url
       (url/url)
       (dissoc :anchor)
-      (update :path #(when (not= "/" %) %))
+      (update :path #(when-not (= "/" %) %))
       (url/map->URL)
       str))
 
@@ -47,8 +43,17 @@
     current
     (str (url/url base-url current))))
 
+(defn- subpage?
+  "Logical true if the `current` url is a subpage of the `base` url.
+   A subpage is at the same level or deeper than `base` when comparing
+   paths. It does assume they belong to the same site, i.g. domain."
+  [base current]
+  (let [base (:path (url/url base)) current (:path (url/url current))]
+    (clojure.string/starts-with? current base)))
+
 (defn- crawl-others
-  "Gets all the a's href urls on `html` content."
+  "Gets all the a's href urls on `html`.
+   Urls from other domains or shallower levels are filtered out."
   [html url]
   (when html
     (let [domain-pattern (re-pattern (escape-dots (:host (url/url url))))]
@@ -56,7 +61,8 @@
            (re-seq #"a\s+href=\"([^\"]+)\"")
            (map last)
            (map (partial make-absolute url))
-           (filter #(re-find domain-pattern %))))))
+           (filter #(re-find domain-pattern %))
+           (filter (partial subpage? url))))))
 
 (defn- parse-site
   "Goes through a site looking for github & twitter urls."
